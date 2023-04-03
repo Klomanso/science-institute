fc9eedf80dfee52274ab9fb7d43c993e5549347a
inspire <k>
2023-04-04 02:09:38 +0300

    Initial commit

---
+++ src/main/java/com/example/scienceinstitute/ScienceInstituteApplication.java
@@ -0,0 +1,13 @@
+package com.example.scienceinstitute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScienceInstituteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScienceInstituteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/scienceinstitute/ScienceInstituteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scienceinstitute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceInstituteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
